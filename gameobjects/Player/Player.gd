944a708466bb6d7dbd56d0c7dc7b8461117b6147
Philipp Kübler <philipp@sourceboat.com>
2018-10-13 16:17:53 +0200

    add player rotations

--- gameobjects/Player/Player.gd
+++ gameobjects/Player/Player.gd
@@ -9,16 +9,17 @@ func _ready():
 	print(position)
 
 func get_input():
-    velocity = Vector2()
-    if Input.is_action_pressed('ui_right'):
-        velocity.x += 1
-    if Input.is_action_pressed('ui_left'):
-        velocity.x -= 1
-    if Input.is_action_pressed('ui_down'):
-        velocity.y += 1
-    if Input.is_action_pressed('ui_up'):
-        velocity.y -= 1
-    velocity = velocity.normalized() * speed
+	velocity = Vector2()
+	if Input.is_action_pressed('ui_right'):
+		velocity.x += 1
+	if Input.is_action_pressed('ui_left'):
+		velocity.x -= 1
+	if Input.is_action_pressed('ui_down'):
+		velocity.y += 1
+	if Input.is_action_pressed('ui_up'):
+		velocity.y -= 1
+	velocity = velocity.normalized() * speed
+	rotation = velocity.angle()
 
 func _physics_process(delta):
     get_input()
